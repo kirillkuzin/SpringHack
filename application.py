7492e7615a71cb00000f8420bb625cc7e9878438
Kirill Kuzin <offkirillkuzin@gmail.com>
2019-04-28 14:39:05 +0300

    Set correct save file directory

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from flask import Flask, Response, request
 import crypto
 from fingerprint import FingerPrint
 
-UPLOAD_FOLDER = "spring-hack/pictures"
+UPLOAD_FOLDER = "tmp/pictures"
 
 application = Flask(__name__)
 
