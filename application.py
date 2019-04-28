5c4796d19f63ef40bb752f9eab822bb7026efe5b
Kirill Kuzin <offkirillkuzin@gmail.com>
2019-04-28 17:39:15 +0300

    Prod

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ from flask import Flask, Response, request
 import crypto
 from fingerprint import FingerPrint
 
-ROOT_DIR = os.path.dirname(__file__)
 UPLOAD_FOLDER = "pictures"
 
 application = Flask(__name__)
@@ -15,17 +14,19 @@ application = Flask(__name__)
 def get_wallet():
     file = request.files['finger']
     filename = UPLOAD_FOLDER + "/" + file.filename
-    file.save(ROOT_DIR + "/" + filename)
+    file.save(os.getcwd() + "/" + filename)
     finger_module = FingerPrint(filename)
     seed_phrase = finger_module.get_seed_phrase()
     private_key, public_key = crypto.generate_wallet(seed_phrase)
-    return_data = json.dumps({
+    response_data = json.dumps({
         "seed": seed_phrase,
         "private_key": private_key,
         "public_key": public_key,
         "balance": crypto.get_balance(public_key)
     })
-    return Response(return_data, mimetype="application/json"), 200
+    response = Response(response_data, mimetype="application/json")
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response, 200
 
 if __name__ == "__main__":
     application.run(
